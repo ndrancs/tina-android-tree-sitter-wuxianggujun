ea6084c3747dd286e9fe2b5b05be05a5519a99f8
wuxianggujun <3344207732@qq.com>
2025-12-29 21:31:41 +0800

    chore: convert cpp and json submodules to regular directories
    These submodules also had local modifications. Converting them ensures
    all grammar code is self-contained in this repository.

---
+++ grammars/cpp/bindings/python/tree_sitter_cpp/__init__.py
@@ -0,0 +1,5 @@
+"Cpp grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ grammars/json/bindings/python/tree_sitter_json/__init__.py
@@ -0,0 +1,5 @@
+"Json grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
